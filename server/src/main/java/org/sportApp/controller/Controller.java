f72b2c069a2a2faadf80c219cd65fd4d5eddbf65
Arina Ivanova <arishkamu@gmail.com>
2024-03-04 01:35:48 +0300

    move server to new prj structer

---
+++ server/src/main/java/org/sportApp/controller/Controller.java
@@ -0,0 +1,27 @@
+package org.sportApp.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+@RequestMapping("/api/users")
+public class Controller {
+    private final List<String> users = new ArrayList<>();
+    @GetMapping
+    public List<String> getAllUsers() {
+        return users;
+    }
+    @PostMapping
+    public String createUser(@RequestBody String user) {
+        users.add(user);
+        return "User created: " + user;
+    }
+    @DeleteMapping("/{user}")
+    public String deleteUser(@PathVariable String user) {
+        if (users.contains(user)) {
+            users.remove(user);
+            return "User deleted: " + user;
+        }
+        return "User not found";
+    }
+}
