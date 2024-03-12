90ece6f9022359c44df15dafe73e4e93e8868e73
true_minmise <dokhru@gmail.com>
2024-03-12 20:28:06 +0300

    unsolved problem with Adapter and Fragment

--- client/app/src/main/java/org/sportApp/userInterface/coach/ui/listOfSportsmen/CoachListOfSportsmenFragment.java
+++ client/app/src/main/java/org/sportApp/userInterface/coach/ui/listOfSportsmen/CoachListOfSportsmenFragment.java
@@ -9,12 +9,21 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import org.sportApp.model.User;
 import org.sportApp.userInterface.coach.databinding.FragmentCoachListOfSportsmenBinding;
+
+import java.util.List;
 
 public class CoachListOfSportsmenFragment extends Fragment {
 
     private FragmentCoachListOfSportsmenBinding binding;
+
+    private RecyclerView userRecyclerView;
+    private UserAdapter userAdapter;
+    private List<User> userList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,9 +32,11 @@ public class CoachListOfSportsmenFragment extends Fragment {
 
         binding = FragmentCoachListOfSportsmenBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-
-        final TextView textView = binding.textCoachListOfSportsmen;
-        coachListOfSportsmenViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //UserAdapter = new UserAdapter(this, userList);
+        //binding.userRecyclerView.setLayoutManager(new LinearLayoutManager());
+        //LinearLayoutManager manager = new LinearLayoutManager();
+        //final TextView textView = binding.textCoachListOfSportsmen;
+        //coachListOfSportsmenViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- client/app/src/main/java/org/sportApp/userInterface/coach/ui/listOfSportsmen/CoachListOfSportsmenViewModel.java
+++ client/app/src/main/java/org/sportApp/userInterface/coach/ui/listOfSportsmen/CoachListOfSportsmenViewModel.java
@@ -3,16 +3,20 @@ package org.sportApp.userInterface.coach.ui.listOfSportsmen;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.sportApp.model.User;
+
+import java.util.List;
 
 public class CoachListOfSportsmenViewModel extends ViewModel {
-    private final MutableLiveData<String> mText;
+    //private RecyclerView userRecyclerView;
+    //private UserAdapter userAdapter;
+    //private List<User> userList;
 
     public CoachListOfSportsmenViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is coach list of sportsmen fragment");
+
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
+
 }
