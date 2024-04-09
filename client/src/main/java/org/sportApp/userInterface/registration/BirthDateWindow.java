c0a7bf06c094b7ce625cc72d478c5c91fb017608
mediana105 <st087217@student.spbu.ru>
2024-04-09 19:59:52 +0300

    Date of birth verification has been added to the Birth Date Window

--- client/src/main/java/org/sportApp/userInterface/registration/BirthDateWindow.java
+++ client/src/main/java/org/sportApp/userInterface/registration/BirthDateWindow.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import org.sportApp.registration.UserRegistrationDto;
@@ -23,19 +25,28 @@ public class BirthDateWindow extends AppCompatActivity {
         Button continueButton = findViewById(R.id.continueButton);
 
         continueButton.setOnClickListener(v -> {
-            Intent intent = getIntent();
-            UserRegistrationDto userDto = (UserRegistrationDto) intent.getSerializableExtra("userDto");
             int year = datePicker.getYear();
             int month = datePicker.getMonth();
             int day = datePicker.getDayOfMonth();
-            Calendar calendar = Calendar.getInstance();
-            calendar.set(year, month, day);
-            Date selectedDate = calendar.getTime();
-            assert userDto != null;
-            userDto.setDateOfBirth(selectedDate);
-            Intent nextIntent = new Intent(BirthDateWindow.this, TypeSelectionWindow.class);
-            nextIntent.putExtra("userDto", userDto);
-            startActivity(nextIntent);
+
+            Calendar selectedCalendar = Calendar.getInstance();
+            selectedCalendar.set(year, month, day);
+
+            Calendar currentCalendar = Calendar.getInstance();
+
+            if (selectedCalendar.getTimeInMillis() <= currentCalendar.getTimeInMillis()) {
+                Intent intent = getIntent();
+                UserRegistrationDto userDto = (UserRegistrationDto) intent.getSerializableExtra("userDto");
+                Date selectedDate = selectedCalendar.getTime();
+                assert userDto != null;
+                userDto.setDateOfBirth(selectedDate);
+                Intent nextIntent = new Intent(BirthDateWindow.this, TypeSelectionWindow.class);
+                nextIntent.putExtra("userDto", userDto);
+                startActivity(nextIntent);
+            } else {
+                Toast.makeText(BirthDateWindow.this, "Please select a valid date", Toast.LENGTH_SHORT).show();
+            }
         });
+
     }
 }
