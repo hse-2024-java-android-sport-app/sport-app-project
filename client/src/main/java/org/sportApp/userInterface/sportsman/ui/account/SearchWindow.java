ae06ef1d0dce5c12631748593237d4277ed24a2e
mediana105 <st087217@student.spbu.ru>
2024-05-07 02:08:40 +0300

    Added search window in side menu

---
+++ client/src/main/java/org/sportApp/userInterface/sportsman/ui/account/SearchWindow.java
@@ -0,0 +1,2 @@
+package org.sportApp.userInterface.sportsman.ui.account;public class SearchWindow {
+}
--- client/src/main/java/org/sportApp/userInterface/sportsman/ui/trainings/TrainingViewModel.java
+++
@@ -1,6 +0,0 @@
-package org.sportApp.userInterface.sportsman.ui.trainings;
-
-import androidx.lifecycle.ViewModel;
-
-public class TrainingViewModel extends ViewModel {
-}
