05a2db0ba1121dc3515759b636b2d620ce32bcdf
mediana105 <st087217@student.spbu.ru>
2024-05-24 15:53:49 +0300

    Added windows to display the training plan

--- client/src/main/java/org/sportApp/userInterface/sportsman/ui/plans/AllPlansWindow.java
+++ client/src/main/java/org/sportApp/userInterface/sportsman/ui/plans/AllPlansWindow.java
@@ -2,11 +2,10 @@ package org.sportApp.userInterface.sportsman.ui.plans;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.TextView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -17,18 +16,22 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import org.sportApp.training.ExerciseDto;
+import org.sportApp.requests.BackendService;
+import org.sportApp.training.TrainingDto;
 import org.sportApp.userInterface.R;
 import org.sportApp.training.PlanDto;
+import org.sportApp.userInterface.sportsman.ui.trainings.OneTrainingWindow;
+import org.sportApp.utils.UserManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlanWindow extends Fragment {
+public class AllPlansWindow extends Fragment {
 
     private boolean isWindowOpened = false;
-    private final List<PlanDto> currentPlanDtos = new ArrayList<>();
-    private final List<PlanDto> completedPlanDtos = new ArrayList<>();
+    private final List<PlanDto> plans = new ArrayList<>();
+    private final List<PlanDto> currentPlans = new ArrayList<>();
+    private final List<PlanDto> completedPlans = new ArrayList<>();
 
     @Nullable
     @Override
@@ -41,23 +44,24 @@ public class PlanWindow extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         if (!isWindowOpened) {
-            PlanDto planDto1 = new PlanDto();
-            planDto1.setName("Test Plan");
-            planDto1.setCompleted(false);
-            currentPlanDtos.add(planDto1);
-
-            for (int i = 0; i < 20; i++) {
-                PlanDto plan = new PlanDto();
-                plan.setName("Completed plan " + i);
-                plan.setCompleted(true);
-                completedPlanDtos.add(plan);
-            }
+//            getAllPlans(UserManager.getInstance().getUserId());
+//            PlanDto planDto1 = new PlanDto();
+//            planDto1.setName("Test Plan");
+//            planDto1.setCompleted(false);
+//            currentPlanDtos.add(planDto1);
+//
+//            for (int i = 0; i < 20; i++) {
+//                PlanDto plan = new PlanDto();
+//                plan.setName("Completed plan " + i);
+//                plan.setCompleted(true);
+//                completedPlanDtos.add(plan);
+//            }
 
             isWindowOpened = true;
         }
 
         RecyclerView currentPlanRecyclerView = view.findViewById(R.id.currentPlanRecyclerView);
-        PlanAdapter currentAdapter = new PlanAdapter(currentPlanDtos, new PlanAdapter.OnItemClickListener() {
+        PlanAdapter currentAdapter = new PlanAdapter(currentPlans, new PlanAdapter.OnItemClickListener() {
             @Override
             public void onItemLongClick(int position) {
             }
@@ -70,13 +74,14 @@ public class PlanWindow extends Fragment {
         currentPlanRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
 
         RecyclerView completedPlansRecyclerView = view.findViewById(R.id.completedPlansRecyclerView);
-        PlanAdapter completedAdapter = new PlanAdapter(completedPlanDtos, new PlanAdapter.OnItemClickListener() {
+        PlanAdapter completedAdapter = new PlanAdapter(completedPlans, new PlanAdapter.OnItemClickListener() {
             @Override
             public void onItemLongClick(int position) {
             }
 
             @Override
             public void onItemClick(int position) {
+                showPlan(position);
             }
         });
         completedPlansRecyclerView.setAdapter(completedAdapter);
@@ -89,4 +94,25 @@ public class PlanWindow extends Fragment {
             PlanDto planDto = result.getData().getParcelableExtra("planDto");
         }
     });
+
+    private void showPlan(int position) {
+        if (position != RecyclerView.NO_POSITION) {
+//            PlanDto completedPlan = completedPlanDtos.get(position);
+//            Log.d("completedPlans", String.valueOf(completedPlanDtos.size()));
+//            Intent intent = new Intent(requireContext(), OnePlanWindow.class);
+//            intent.putExtra("planDto", completedPlan);
+//            startActivity(intent);
+        }
+    }
+
+//    private void getAllTrainings(Long userId) {
+//        BackendService.getAllPlans(userId).thenAccept(resultDto -> {
+//                    plans = resultDto;
+//                    Log.d("UserType", "resultDto: " + resultDto);
+//                })
+//                .exceptionally(e -> {
+//                    //Toast.makeText(TrainingsWindow.this, "Authorization failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+//                    return null;
+//                }).join();
+//    }
 }
