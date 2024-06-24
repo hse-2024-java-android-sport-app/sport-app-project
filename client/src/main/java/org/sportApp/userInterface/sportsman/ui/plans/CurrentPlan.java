ef1e244174fc9ee39efb23ab84f4cfe6f85e37fb
mediana105 <st087217@student.spbu.ru>
2024-06-25 01:51:18 +0300

    The coach opens the necessary window to create a plan

--- client/src/main/java/org/sportApp/userInterface/sportsman/ui/plans/CurrentPlan.java
+++ client/src/main/java/org/sportApp/userInterface/sportsman/ui/plans/CurrentPlan.java
@@ -55,7 +55,7 @@ public class CurrentPlan extends FragmentWithAddButton<TrainingEventDto> {
 
     @Override
     protected Class<?> getShowWindowClass() {
-        return OneEvent.class;
+        return CreatePlan.class;
     }
 
     @Override
